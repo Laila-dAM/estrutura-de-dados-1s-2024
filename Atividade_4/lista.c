d615ccf417e9417819e2fd59507b332f4d040ddf
Laila-dAM <lailamota.238@gmail.com>
2024-04-20 00:00:44 -0300

    Essa função insere um novo nó com um dado especifico no final da lista, tratando os erros

--- Atividade_4/lista.c
+++ Atividade_4/lista.c
@@ -15,3 +15,21 @@ No* lista_criar(){
     }
 }
 
+void lista_inserir_final(No* L, char dado) {
+    No* novo = (No*)malloc(sizeof(No));
+    if (novo != NULL){
+        novo->dado = dado;
+        novo->proximo = NULL;
+        No*atual = L;
+        while (atual->proximo != NULL){
+            atual = atual->proximo;
+        }
+        atual->proximo = novo;
+    }
+
+    else {
+        printf("Erro: não foi possível alocar memória para o novo nó.\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
