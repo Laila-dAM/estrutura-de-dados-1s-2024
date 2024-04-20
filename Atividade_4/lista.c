446426fbfd69fb8cfb049e9339b3681141a0a828
Laila-dAM <lailamota.238@gmail.com>
2024-04-19 23:54:21 -0300

    Essa função cria uma nova lista encadeada, alocando memória para o nó cabeça e tratando possiveis falhas de alocação

--- Atividade_4/lista.c
+++ Atividade_4/lista.c
@@ -2,3 +2,16 @@
 #include <stdlib.h>
 #include "lista.h"
 
+No* lista_criar(){
+    No* cabeca = (No*)malloc(sizeof(No));
+    if (cabeca != NULL){
+        cabeca->proximo = NULL;
+        refurn cabeca;
+    }
+
+    else {
+        printf("Erro: não foi possível alocar memória para a lista.\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
