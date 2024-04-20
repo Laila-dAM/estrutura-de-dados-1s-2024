c2c671151a9798754fa8c08f459eb1d0a804eb4d
Laila-dAM <lailamota.238@gmail.com>
2024-04-19 23:46:59 -0300

    Declaraçoes de funções para manipulação de lista encadeada

--- Atividade_4/lista.h
+++ Atividade_4/lista.h
@@ -6,3 +6,16 @@ typedef struct No {
     struct No* proximo;
 
 } No;
+
+No* lista_criar();
+void lista_inserir_final(No* L, char dado);
+void lista_imprimir(No* L);
+int lista_verificar_existencia(No*L, char valor_busca);
+int lista_verificar_ocorrencias(No* L, char valor_busca);
+void lista_imprimir_inversa(No* L);
+void lista_inserir_no_i(No* L, int i, char dado);
+void lista_remover_no_i(No* L, int i);
+void lista_remover_no(No* L, char valor_busca);
+void lista_destruir(No* L);
+
+#endif
