93a360f5726eb9371d39284f2c3d8ceb8016a143
Laila-dAM <lailamota.238@gmail.com>
2024-04-26 21:22:25 -0300

    Insere um novo nó com valor de lista

--- Atividade_5/lista_circular.c
+++ Atividade_5/lista_circular.c
@@ -5,4 +5,15 @@
 void inserir_no(No** inicio, char valor) {
     No* novo_no = (No*)malloc(sizeof(No));
     novo_no->valor = valor;
+
+    if (*inicio == NULL){
+        *inicio = novo_no;
+        novo_no->proximo = novo_no;
+    } 
+    else {
+        No* temp = *inicio;
+        while(temp->proximo != *inicio){
+            temp = temp->proximo;
+        }
+    }
 }
