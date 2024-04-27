b3d97aea6480061139e44715bbd92ad6da780771
Laila-dAM <lailamota.238@gmail.com>
2024-04-27 12:00:37 -0300

    Conta o número de linhas no arquivo e aloca dinamicamente memória para armazenar os pontos

--- Trabalho_1/regressao_linear.c
+++ Trabalho_1/regressao_linear.c
@@ -16,4 +16,16 @@ int main(int argc, char *argc[]){
         printf("Erro ao abrir o arquivo.\n");
         return 1;
     }
+
+    int num_pontos = 0;
+    char linha[100];
+    while (fgets(linha, sizeof(linha), arquivo) != NULL){
+        num_pontos++;
+    }
+    rewind(arquivo);
+    Ponto *pontos = malloc(num_pontos *sizeof(Ponto));
+    if (pontos == NULL){
+        printf("Erro de alocação de memória.\n");
+        return 1;
+    }
 }
