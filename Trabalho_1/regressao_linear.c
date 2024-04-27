692256008c5cb788b98b12718b1494cc590e3d43
Laila-dAM <lailamota.238@gmail.com>
2024-04-27 00:56:15 -0300

    Arquivo de dados.csv adicionado e funções básicas de e/s definem uma estrutura para pontos

--- Trabalho_1/regressao_linear.c
+++ Trabalho_1/regressao_linear.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+    int x;
+    float y;
+} Ponto;
+
+int main(int argc, char *argc[]){
+    if (argc != 2){
+        printf("Uso: %s <arquivo.csv>\n", argv[0]);
+        return 1;
+    }
+}
