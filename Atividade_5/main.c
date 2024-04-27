a8f0f50a279fd85504e813b96dbc6aaa98217587
Laila-dAM <lailamota.238@gmail.com>
2024-04-26 21:09:00 -0300

    Cria, modifica e exibe uma lista circular, removendo um elemento e liberando a mémoria

--- Atividade_5/main.c
+++ Atividade_5/main.c
@@ -3,5 +3,23 @@
 
 int main(){
     No* inicio = NULL;
-    
+
+inserir_no(&inicio, 'A');
+    inserir_no(&inicio, 'B');
+    inserir_no(&inicio, 'C');
+    inserir_no(&inicio, 'D');
+
+    printf("Lista circular: ");
+    imprimir_lista(inicio);
+    printf("\n");
+
+    remover_no(&inicio, 'B');
+
+    printf("Lista após remover 'B': ");
+    imprimir_lista(inicio);
+    printf("\n");
+
+    liberar_lista(&inicio);
+
+    return 0;
 }
