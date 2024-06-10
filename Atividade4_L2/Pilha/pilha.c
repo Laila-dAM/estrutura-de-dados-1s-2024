1bc5b68c29bfdffa358f8cbf874320d435e3b717
Laila-dAM <lailamota.238@gmail.com>
2024-06-10 18:50:59 -0300

    Lidam com operações básicas de uma pilha como destruição, verificação se está vazia e se está cheia

--- Atividade4_L2/Pilha/pilha.c
+++ Atividade4_L2/Pilha/pilha.c
@@ -9,3 +9,16 @@ Pilha* criarPilha(int capacidade) {
     pilha->dados = (int *)malloc(capacidade * sizeof(int));
     return pilha;
 }
+
+void destruirPilha(Pilha *pilha) {
+    free(pilha->dados);
+    free(pilha);
+}
+
+int estaVazia(Pilha *pilha) {
+    return pilha->topo == -1;
+}
+
+int estaCheia(Pilha *pilha) {
+    return pilha->topo == pilha->capacidade - 1;
+}
