42ac50b619f0cac420dd02732b5481de55271851
Laila-dAM <lailamota.238@gmail.com>
2024-06-10 19:18:46 -0300

    Implementa funções para criar uma fila e adicionar um elemento à fila

---
+++ Atividade4_L2/Fila/fila.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fila.h"
+
+Queue* createQueue() {
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q->front = q->rear = NULL;
+    return q;
+}
+
+void enqueue(Queue* q, int value) {
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->data = value;
+    temp->next = NULL;
+    if (q->rear == NULL) {
+        q->front = q->rear = temp;
+        return;
+    }
