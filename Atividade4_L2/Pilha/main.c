b0019411be1dd418a7edb3115afaf1c1b50e72ed
Laila-dAM <lailamota.238@gmail.com>
2024-06-10 18:59:32 -0300

    Exibe um menu com opções para interagir com uma pilha.

--- Atividade4_L2/Pilha/main.c
+++ Atividade4_L2/Pilha/main.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pilha.h"
+
+void exibirMenu() {
+    printf("\nMenu:\n");
+    printf("1. Empilhar\n");
+    printf("2. Desempilhar\n");
+    printf("3. Exibir Topo\n");
+    printf("4. Sair\n");
+    printf("Escolha uma opção: ");
+}
