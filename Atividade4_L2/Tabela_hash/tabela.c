9d0b65caaed3139738d68bdbce8cfb4fff8ef6e0
Laila-dAM <lailamota.238@gmail.com>
2024-06-10 19:50:41 -0300

    Excluir elementos da tabela hash, exibri a tabela e liberar sua memória

--- Atividade4_L2/Tabela_hash/tabela.c
+++ Atividade4_L2/Tabela_hash/tabela.c
@@ -31,4 +31,53 @@ int search(HashTable* ht, int key) {
         if (node->key == key) {
             return node->value;
         }
+   node = node->next;
+    }
+    return -1; // Indica que a chave não foi encontrada
+}
+
+void delete(HashTable* ht, int key) {
+    int hashIndex = hashFunction(key);
+    Node* node = ht->table[hashIndex];
+    Node* prev = NULL;
+    while (node != NULL && node->key != key) {
+        prev = node;
+        node = node->next;
+    }
+    if (node == NULL) {
+        printf("Chave %d não encontrada.\n", key);
+        return;
+    }
+    if (prev == NULL) {
+        ht->table[hashIndex] = node->next;
+    } else {
+        prev->next = node->next;
+    }
+    free(node);
+    printf("Chave %d removida.\n", key);
+}
+
+void displayTable(HashTable* ht) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        printf("Índice %d: ", i);
+        Node* node = ht->table[i];
+        while (node != NULL) {
+            printf("-> [Chave: %d, Valor: %d] ", node->key, node->value);
+            node = node->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+void freeTable(HashTable* ht) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        Node* node = ht->table[i];
+        while (node != NULL) {
+            Node* temp = node;
+            node = node->next;
+            free(temp);
+        }
+    }
+    free(ht);
+}
 
