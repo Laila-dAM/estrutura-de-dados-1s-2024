3168ece96792f3369901cb3be44c4efa1e08093d
Laila-dAM <lailamota.238@gmail.com>
2024-06-10 19:40:50 -0300

    Manipula a estrutura, incluindo inserção, busca, exclusão, exibição e liberação

--- Atividade4_L2/Tabela_hash/tabela.h
+++ Atividade4_L2/Tabela_hash/tabela.h
@@ -8,3 +8,14 @@ typedef struct Node {
     int value;
     struct Node* next;
 } Node;
+
+typedef struct HashTable {
+    Node* table[TABLE_SIZE];
+} HashTable;
+
+HashTable* createTable();
+void insert(HashTable* ht, int key, int value);
+int search(HashTable* ht, int key);
+void delete(HashTable* ht, int key);
+void displayTable(HashTable* ht);
+void freeTable(HashTable* ht);
