a9ec89878e7339647acbb2289f66e51a30d88eec
Laila-dAM <lailamota.238@gmail.com>
2024-06-10 19:53:27 -0300

    Menu, para exibir opções de interação com uma tabela hash, como inserir, buscar e remover e elementos

---
+++ Atividade4_L2/Tabela_hash/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tabela.h"
+
+void menu() {
+    printf("\n--- Menu ---\n");
+    printf("1. Inserir\n");
+    printf("2. Buscar\n");
+    printf("3. Remover\n");
