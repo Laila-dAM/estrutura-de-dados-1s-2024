c116f95d52d3d0c8838a0042e9f9fcba5819d34b
Laila-dAM <lailamota.238@gmail.com>
2024-06-10 19:27:18 -0300

    Menu, exibe opções para interagir com uma fila: enfileirar, desenfileirar, exibir a fila ou sair do programa

---
+++ Atividade4_L2/Fila/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fila.h"
+
+void menu() {
+    printf("\n--- Menu ---\n");
+    printf("1. Enfileirar\n");
+    printf("2. Desenfileirar\n");
+    printf("3. Exibir Fila\n");
+    printf("4. Sair\n");
+    printf("Escolha uma opcao: ");
+}
+
