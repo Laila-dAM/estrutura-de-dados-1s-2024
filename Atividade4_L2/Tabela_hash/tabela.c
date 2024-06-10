a1ac1dc5b8176389d214e4c81e212e9512348344
Laila-dAM <lailamota.238@gmail.com>
2024-06-10 19:47:36 -0300

    Inserir e buscar elementos em uma tabela hash

--- Atividade4_L2/Tabela_hash/tabela.c
+++ Atividade4_L2/Tabela_hash/tabela.c
@@ -11,3 +11,24 @@ HashTable* createTable() {
     for (int i = 0; i < TABLE_SIZE; i++) {
         ht->table[i] = NULL;
     }
+    
+    return ht;
+}
+
+void insert(HashTable* ht, int key, int value) {
+    int hashIndex = hashFunction(key);
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->key = key;
+    newNode->value = value;
+    newNode->next = ht->table[hashIndex];
+    ht->table[hashIndex] = newNode;
+}
+
+int search(HashTable* ht, int key) {
+    int hashIndex = hashFunction(key);
+    Node* node = ht->table[hashIndex];
+    while (node != NULL) {
+        if (node->key == key) {
+            return node->value;
+        }
+
